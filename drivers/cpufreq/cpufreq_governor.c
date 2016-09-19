687f058663206ba5064dafe5cce9f50095cd1aee
GuneetAtwal <guneetatwal@gmail.com>
2016-09-19 19:14:38 +0530

    cpufreq: get rif of huawei crap
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -22,10 +22,6 @@
 
 #include "cpufreq_governor.h"
 
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-#include <power/msgnotify.h>
-#endif
-
 static struct attribute_group *get_sysfs_attr(struct dbs_data *dbs_data)
 {
 	if (have_governor_per_policy())
@@ -43,10 +39,6 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 	unsigned int max_load = 0;
 	unsigned int ignore_nice;
 	unsigned int j;
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-	u64 now_msg_timestamp;
-	unsigned int active_time;
-#endif
 
 	if (dbs_data->cdata->governor == GOV_ONDEMAND)
 		ignore_nice = od_tuners->ignore_nice_load;
@@ -104,15 +96,7 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 		if (unlikely(!wall_time || wall_time < idle_time))
 			continue;
 
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-		now_msg_timestamp = kcpustat_cpu(j).cpustat[CPUTIME_MESSAGE];
-		active_time = (unsigned int)adjust_active_time_by_msg(j, (wall_time - idle_time),
-			wall_time, (now_msg_timestamp - j_cdbs->cputime_msg_timestamp));
-		load = 100 * active_time / wall_time;
-		j_cdbs->cputime_msg_timestamp = now_msg_timestamp;
-#else
 		load = 100 * (wall_time - idle_time) / wall_time;
-#endif
 
 		if (load > max_load)
 			max_load = load;
@@ -338,10 +322,6 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			if (ignore_nice)
 				j_cdbs->prev_cpu_nice =
 					kcpustat_cpu(j).cpustat[CPUTIME_NICE];
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-			j_cdbs->cputime_msg_timestamp =
-				kcpustat_cpu(cpu).cpustat[CPUTIME_MESSAGE];
-#endif
 
 			mutex_init(&j_cdbs->timer_mutex);
 			INIT_DEFERRABLE_WORK(&j_cdbs->work,
