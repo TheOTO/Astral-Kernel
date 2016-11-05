98496e8d6d14029cf0365bf2c1179a5a95181140
MEGAX91 <blackdeath1370@gmail.com>
2016-11-05 10:59:19 +0330

    add toggles for wakelocks

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -15,6 +15,20 @@
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
+
+static bool enable_si_ws = true;
+module_param(enable_si_ws, bool, 0644);
+static bool enable_msm_hsic_ws = true;
+module_param(enable_msm_hsic_ws, bool, 0644);
+static bool wlan_rx_wake = true;
+module_param(wlan_rx_wake, bool, 0644);
+static bool wlan_ctrl_wake = true;
+module_param(wlan_ctrl_wake, bool, 0644);
+static bool wlan_wake = true;
+module_param(wlan_wake, bool, 0644);
+static bool enable_bluedroid_timer_ws = true;
+module_param(enable_bluedroid_timer_ws, bool, 0644);
 
 #include "power.h"
 
@@ -381,6 +395,24 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
+	
+	if (!enable_si_ws && !strcmp(ws->name, "sensor_ind"))
+		return;
+
+	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host"))
+	return;
+
+	if (!wlan_rx_wake && !strcmp(ws->name, "wlan_rx_wake"))
+	return;
+
+	if (!wlan_ctrl_wake && !strcmp(ws->name, "wlan_ctrl_wake"))
+	return;
+
+	if (!wlan_wake && !strcmp(ws->name, "wlan_wake"))
+	return;
+
+	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+		return;
 
 	/*
 	 * active wakeup source should bring the system
@@ -1075,3 +1107,4 @@ static int __init wakeup_sources_debugfs_init(void)
 }
 
 postcore_initcall(wakeup_sources_debugfs_init);
+
