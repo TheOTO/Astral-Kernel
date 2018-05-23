6c38041984295bafbee3575ea6ea1e22da2daec9
TheOTO <theoto777@gmail.com>
2018-05-23 14:29:26 -0400

    Backlight dimmer + min/max levels
    Credits to andip71 (Boeffla Kernel)
    
    Signed-off-by: TheOTO <theoto777@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -74,6 +74,20 @@ struct msmfb_cabc_config g_cabc_cfg_foresd;
 #endif
 
 #define MAX_FBI_LIST 32
+
+#define MDSS_BRIGHT_TO_BL_DIMMER(out, v) do {\
+				out = (((v) - 2) * 255 / 250);\
+				} while (0)
+
+bool backlight_dimmer = false;
+module_param(backlight_dimmer, bool, 0755);
+
+int backlight_min = 0;
+int backlight_max = 255;
+
+module_param(backlight_min, int, 0755);
+module_param(backlight_max, int, 0755);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
@@ -269,10 +283,27 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	if (value > mfd->panel_info->brightness_max)
 		value = mfd->panel_info->brightness_max;
 
-	/* This maps android backlight level 0 to 255 into
-	   driver backlight level 0 to bl_max with rounding */
-	MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
-				mfd->panel_info->brightness_max);
+	// Boeffla: apply min/max limits for LCD backlight (0 is exception for display off)
+	if (value != 0)
+	{
+		if (value < backlight_min)
+			value = backlight_min;
+
+		if (value > backlight_max)
+			value = backlight_max;
+	}
+
+	if (backlight_dimmer) {
+		if (value < 3)
+			bl_lvl = 1;
+		else
+			MDSS_BRIGHT_TO_BL_DIMMER(bl_lvl, value);
+	} else {
+		/* This maps android backlight level 0 to 255 into
+		   driver backlight level 0 to bl_max with rounding */
+		MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
+					mfd->panel_info->brightness_max);
+	}
 
 	if (!bl_lvl && value)
 		bl_lvl = 1;
