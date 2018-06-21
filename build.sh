3f8842dc389e855190f06472ab5e8869e079cb23
TheOTO <theoto777@gmail.com>
2018-06-20 23:14:54 -0400

    Rebrand the project

--- build.sh
+++ build.sh
@@ -12,42 +12,41 @@ blue='\033[0;34m'
 default='\033[0m'
 
 # Resources
-ANDROID_DIR=/home/guneetatwal/android
+ANDROID_DIR=~/android
 KERNEL_DIR=$PWD
 IMAGE=$KERNEL_DIR/arch/arm64/boot/Image
-#IMAGE=$KERNEL_DIR/arch/arm/boot/zImage for 32 bit architecture
 DTBTOOL=$KERNEL_DIR/scripts/dtbTool
-TOOLCHAIN=$ANDROID_DIR/toolchain/aarch64-linux-android-4.9-aosp/bin
+TOOLCHAIN=$ANDROID_DIR/ubertc/bin
 
 #Paths
 OUT_DIR=$KERNEL_DIR/out
-OUT_ZIP=$KERNEL_DIR/Blaze-Releases
+OUT_ZIP=$KERNEL_DIR/Releases
 NEW_OUT=$OUT_DIR/tools
 
 # Kernel Version Info
-BASE="-Blaze™Kernel"
-CUR_VER=""
-BLAZE_VER="$BASE$CUR_VER"
+BASE="-Astral™Kernel"
+CUR_VER="-6.0"
+AK_VER="$BASE$CUR_VER"
  
 
 # Variables
 
-DEFCONFIG="blaze_defconfig"
-export LOCALVERSION=~`echo $BLAZE_VER`
+DEFCONFIG="ak_defconfig"
+export LOCALVERSION=~`echo $AK_VER`
 export CROSS_COMPILE=$TOOLCHAIN/aarch64-linux-android-
 export ARCH=arm64
-export KBUILD_BUILD_USER="GuneetAtwal"
-export KBUILD_BUILD_HOST="BeastPC"
+export KBUILD_BUILD_USER="TheOTO"
+export KBUILD_BUILD_HOST="XDA-Developers"
 
-function make_blaze {
+function make_ak {
 		echo -e "$green*******************************************************"
-		echo "                  Compiling $BLAZE_VER	              "
+		echo "                  Compiling $AK_VER	              "
 		echo -e "*****************************************************"
 		echo
 		make $DEFCONFIG
 		make menuconfig
-		make -j5
-		rm -rf $NEWOUT/Image
+		make -j2
+		rm -rf $NEW_OUT/Image
 		cp -vr $IMAGE $NEW_OUT
 		make_dtb
 		make_zip
@@ -62,18 +61,17 @@ function make_clean {
 		echo "          Cleaning up object files and other stuff	              "
 		echo -e "***********************************************$default"
 		make mrproper
-		make_blaze
+		make_ak
 	}
 		
 function make_recompile {
 			echo -e "$cyan*******************************************************"
-			echo "             Recompiling $BLAZE_VER	              "
+			echo "             Recompiling $AK_VER	              "
 			echo -e "*****************************************************"
-			make -j5
-			rm -rf $NEWOUT/Image
+			make -j2
+			rm -rf $NEW_OUT/Image
 			cp -vr $IMAGE $NEW_OUT
 			make_dtb
-			strip_modules
 			make_zip
 			housekeeping
 		}
@@ -82,8 +80,8 @@ function make_dtb {
 			echo -e "$blue*******************************************************"
 			echo "             		Creating dt.img....	              "
 			echo -e "*****************************************************"
-			rm -rf $NEWOUT/dt.img
-			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm/boot/dts/
+			rm -rf $NEW_OUT/dt.img
+			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm64/boot/dts/
 		}
 
 function make_zip {
@@ -92,9 +90,9 @@ function make_zip {
 		echo -e "*****************************************************"
 		cd $OUT_DIR
 		rm -f '*.zip'
-		zip -yr BlazeKernel`echo $CUR_VER`.zip *
-		mv BlazeKernel`echo $CUR_VER`.zip $OUT_ZIP
-		echo "       Find your zip in Blaze-Releases directory"
+		zip -yr AstralKernel`echo $CUR_VER`.zip *
+		mv AstralKernel`echo $CUR_VER`.zip $OUT_ZIP
+		echo "       Find your zip in Releases directory"
 		echo -e "$default"
 		cd $KERNEL_DIR 
 		}
@@ -113,7 +111,7 @@ while read -p " 'Y' to Compile all , 'R' to clean and recompile , 'C' to to do a
 do
 case "$choice" in
 	y|Y)
-		make_blaze
+		make_ak
 		break
 		;;
 	r|R )
@@ -129,7 +127,7 @@ case "$choice" in
 		;;
 	* )
 		echo
-		echo "Are you drunk???"
+		echo "Wrong input"
 		echo
 		;;
 esac
