4a11dc73de532da2c6fc3a46642d1312e04675ae
TheOTO <theoto777@gmail.com>
2018-06-24 21:42:37 -0400

    Fix flash_kernel script

--- out/tools/flash_kernel.sh
+++ out/tools/flash_kernel.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
  #
- # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com> 
+ # Copyright ï¿½ 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
  #
  # Live ramdisk patching script
  #
@@ -15,7 +15,7 @@
  #
  # Please maintain this if you use this script or any part of it
  #
-cd /tmp/
+cd /tmp/ &&
 /sbin/busybox dd if=/dev/block/bootdevice/by-name/boot of=./boot.img
 ./unpackbootimg -i /tmp/boot.img
 ./mkbootimg --kernel /tmp/Image --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk"  --base 0x80000000 --pagesize 2048 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt /tmp/dt.img -o /tmp/newboot.img
