ecfbf30089a9fdf7dd405b4c1d3c715c6e83cc04
TheOTO <theoto777@gmail.com>
2018-08-17 23:04:55 -0400

    Version 6.0.3
    Switch to AnyKernel2 for build and clean build script

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@
 # Constants
 green='\033[01;32m'
 red='\033[01;31m'
-blink_red='\033[05;31m'
 cyan='\033[0;36m'
 yellow='\033[0;33m'
 blue='\033[0;34m'
@@ -19,13 +18,12 @@ DTBTOOL=$KERNEL_DIR/scripts/dtbTool
 TOOLCHAIN=$ANDROID_DIR/ubertc/bin
 
 #Paths
-OUT_DIR=$KERNEL_DIR/out
+OUT_DIR=$KERNEL_DIR/AnyKernel2
 OUT_ZIP=$KERNEL_DIR/Releases
-NEW_OUT=$OUT_DIR/tools
 
 # Kernel Version Info
-BASE="-Astral-Kernel"
-CUR_VER="-6.0.2"
+BASE="Astral-Kernel"
+CUR_VER="-6.0.3"
 AK_VER="$BASE$CUR_VER"
 
 
@@ -41,83 +39,67 @@ export KBUILD_BUILD_USER="TheOTO"
 export KBUILD_BUILD_HOST="XDA-Developers"
 
 function make_ak {
-		echo -e "$green*******************************************************"
-		echo "                  Compiling $AK_VER	              "
-		echo -e "*****************************************************"
+		echo -e "${green}"
+		echo "**********    Compiling $AK_VER    **********"
 		echo
 		make $DEFCONFIG
 		make menuconfig
 		make -j2
-		rm -rf $NEW_OUT/Image
-		cp -vr $IMAGE $NEW_OUT
+		rm -rf $OUT_DIR/Image
+		cp -vr $IMAGE $OUT_DIR
 		make_dtb
 		make_zip
 		housekeeping
-		echo -e "$green*******************************************************"
-		echo "              Compilation Completed!!              "
-		echo -e "*****************************************************$default"
-		}
+		echo -e "${green}"
+		echo "**********    Build complete!    **********"
+		echo -e "${default}"
+}
 
 function make_clean {
-		echo -e "$green***********************************************"
-		echo "          Cleaning up object files and other stuff	              "
-		echo -e "***********************************************$default"
-		make mrproper
-		make_ak
-	}
-
-function make_recompile {
-			echo -e "$cyan*******************************************************"
-			echo "             Recompiling $AK_VER	              "
-			echo -e "*****************************************************"
-			make -j2
-			rm -rf $NEW_OUT/Image
-			cp -vr $IMAGE $NEW_OUT
-			make_dtb
-			make_zip
-			housekeeping
-		}
+		echo -e "${green}"
+		echo "**********    Cleaning up the build    **********"
+		echo -e "${default}"
+		echo
+		make clean && make mrproper
+		git clean -f -d > /dev/null 2>&1
+}
 
 function make_dtb {
-			echo -e "$blue*******************************************************"
-			echo "             		Creating dt.img....	              "
-			echo -e "*****************************************************"
-			rm -rf $NEW_OUT/dt.img
-			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm64/boot/dts/
-		}
+		echo -e "${blue}"
+		echo "**********    Creating dt.img...    **********"
+		rm -rf $OUT_DIR/dt.img
+		$DTBTOOL -v -s 2048 -o $OUT_DIR/dt.img -p scripts/dtc/ arch/arm64/boot/dts/
+}
 
 function make_zip {
-		echo -e "$yellow*******************************************************"
-		echo "             		Zipping up....	              "
-		echo -e "*****************************************************"
+		echo -e "${yellow}"
+		echo "**********    Zipping up...    **********"
 		cd $OUT_DIR
 		rm -f '*.zip'
 		zip -yr Astral-Kernel`echo $CUR_VER`.zip *
 		mv Astral-Kernel`echo $CUR_VER`.zip $OUT_ZIP
-		echo "       Find your zip in Releases directory"
-		echo -e "$default"
+		echo -e "${default}"
+		echo -e "${red}"
+		echo "+++   Find your zip in Releases directory   +++"
+		echo -e "${default}"
 		cd $KERNEL_DIR
-		}
+}
 
 function housekeeping {
-		echo -e "$green*******************************************************"
-		echo "            Cleaning up the mess created...	              "
-		echo -e "*****************************************************$default"
-		rm -rf $NEW_OUT/Image
-		rm -rf $NEW_OUT/dt.img
+		echo -e "${green}"
+		echo "**********    Cleaning up the mess created...    **********"
+		echo -e "${default}"
+		rm -rf $OUT_DIR/Image
+		rm -rf $OUT_DIR/dt.img
 	}
 
 
 BUILD_START=$(date +"%s")
-while read -p " 'Y' to Compile all , 'R' to clean and recompile , 'C' to to do a clean compilation 'N' to exit " choice
+while read -p " Choose 'Y' to compile all , 'C' to clean all , or 'N' to exit = " choice
 do
 case "$choice" in
 	y|Y)
 		make_ak
-		break
-		;;
-	r|R )
-		make_recompile
 		break
 		;;
 	c|C )
@@ -129,11 +111,13 @@ case "$choice" in
 		;;
 	* )
 		echo
-		echo "Wrong input"
+		echo "Invalid selection"
 		echo
 		;;
 esac
 done
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$default"
+echo -e "${green}"
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo -e "${default}"
