bd5f7e12c00d57371c3ea3e8fe2082f27e33c73d
TheOTO <theoto777@gmail.com>
2018-08-27 18:35:13 -0400

    Astral: ramdisk: modify the ramdisk a bit

--- AnyKernel2/ramdisk/init.special_power.sh
+++ AnyKernel2/ramdisk/init.special_power.sh
@@ -60,22 +60,6 @@ write /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq 400000
 write /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq 400000
 write /sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq 400000
 
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/above_hispeed_delay 0
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/go_hispeed_load 90
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/min_sample_time 40000
-write /sys/devices/system/cpu/cpu0/cpufreq/target_loads "80 345600:33 400000:25 533330:50 800000:65 960000:70 1113600:85 134400:90 1459200:92 1497600:98"
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/timer_rate 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/impulse/timer_slack -1
-
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/above_hispeed_delay 0
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/go_hispeed_load 95
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/min_sample_time 30000
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/target_loads "80 249600:25 40000:50 499200:65 800000:70 998400:85 1113600:90 1209600:98"
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/timer_rate 30000
-write /sys/devices/system/cpu/cpu4/cpufreq/impulse/timer_slack -1
-
 write /proc/sys/kernel/sched_rt_period_us 1000000
 write /proc/sys/kernel/sched_rt_runtime_us 800000
 write /proc/sys/kernel/sched_wake_to_idle 0
@@ -85,7 +69,7 @@ write /dev/cpuctl/cpu.rt_runtime_us 800000
 write /sys/module/msm_performance/parameters/touchboost 0
 write /sys/module/cpu_boost/parameters/input_boost_freq "0:400000 1:400000 2:400000 3:400000 4:800000 5:800000 6:800000 7:800000"
 write /sys/module/cpu_boost/parameters/boost_ms 0
-write /sys/module/cpu_boost/parameters/input_boost_ms 1500
+write /sys/module/cpu_boost/parameters/input_boost_ms 500
 write /sys/module/cpu_boost/parameters/sync_threshold 400000
 write /sys/module/cpu_boost/parameters/wakeup_boost Y
 write /sys/module/cpu_boost/parameters/hotplug_boost Y
